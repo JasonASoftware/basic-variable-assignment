c07741909c1a54c658ffcf6233bdf7b5659ee71d
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-11-17 18:11:12 -0500

    created js file and added code

---
+++ basicVariable.js
@@ -0,0 +1,9 @@
+//default code (wrong code)
+// var a == "code";
+// var b == "wa.rs";
+// var name == a + b;
+
+//right code to debug it you would have to take out a "=" to make it true to print out
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
